a5e381b7ddd50a4a8633d8d7a90b8e503deb0ebb
Вячеслав Гаврилюк <vvgavrilyuk@hotmail.com>
2023-03-01 15:10:36 +0300

    Добавил ввод с клавиатуры

--- TaskConsole7CLR/TaskConsole7CLR.cpp
+++ TaskConsole7CLR/TaskConsole7CLR.cpp
@@ -5,19 +5,26 @@ using namespace System::IO;
 
 int main(array<System::String ^> ^args)
 {
-    String^ path = "C:\\Users\\vvgav\\Pictures";
+    
+    //String^ path = "C:\\Users\\vvgav\\Pictures";
+    Console::Write("Введите директорию: ");
+    String^ path = Console::ReadLine();
+    Console::WriteLine("");
+    Console::Write("Введите размер файла: ");
+    String^ size = Console::ReadLine();
     array <System::String^> ^files = Directory::GetFiles(path);
     
     int j = 0;
     for (int i = 0; i < files->Length; i++)
     {
         FileInfo^ fileInfo = gcnew FileInfo(files->GetValue(i)->ToString());         
-        if (fileInfo->Length > 500000) 
+        if (fileInfo->Length > Convert::ToInt32(size))
         {
             Console::WriteLine("Name: {0}", fileInfo->Name);
             j++;
         }       
     }
     Console::WriteLine("Колличество файлов: {0}", j);
+    Console::ReadLine();
     return 0;
 }
